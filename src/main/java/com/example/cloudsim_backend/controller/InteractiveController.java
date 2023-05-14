7be8e04e15b83ffbea840ec884b1d090287682a9
MingzhenHan <hanmingzhen2002@outlook.com>
2023-05-14 17:52:35 +0800

    add upload file api

--- src/main/java/com/example/cloudsim_backend/controller/InteractiveController.java
+++ src/main/java/com/example/cloudsim_backend/controller/InteractiveController.java
@@ -1,18 +1,42 @@
 package com.example.cloudsim_backend.controller;
 
 import com.example.cloudsim_backend.service.BackendFunction;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 
 @RestController
+@CrossOrigin
 public class InteractiveController {
     @Autowired
     private BackendFunction backendFunction;
 
     @GetMapping("/getGraph")
     public Graph getGraph(){
-        return backendFunction.processGraph();
+        Graph graph = new Graph();
+        graph.setA(5);
+        graph.setB("qing");
+        return graph;
+    }
+
+    @PostMapping("/uploadfile")
+    public Result uploadFile(MultipartFile file, HttpServletRequest req) {
+        Result result= new Result();
+
+        try {
+            file.transferTo(new File("D:\\projects\\cloudsim_backend\\inputfile", file.getOriginalFilename()));
+            result.setStatus(true);
+            result.setMsg("file upload success");
+        } catch (IOException e) {
+            result.setStatus(false);
+            result.setMsg(e.getMessage());
+        }
+        return result;
     }
 }
---
+++ src/main/java/com/example/cloudsim_backend/controller/Result.java
@@ -0,0 +1,22 @@
+package com.example.cloudsim_backend.controller;
+
+public class Result {
+    private boolean status;
+    private String msg;
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
